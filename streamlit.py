4959690a583d536670d252abd52ab2b28ef535f9
HrexD <cedgoujon78@gmail.com>
2022-10-25 11:39:33 +0200

    '25/10'

--- streamlit.py
+++ streamlit.py
@@ -7,4 +7,5 @@ import streamlit as st
 warnings.filterwarnings('ignore')
 
 df=pd.read_csv(r'.\Twitch_game_data.csv',encoding='cp1252')
-df
+df=df.query('Year==2021 and Month==12')
+st.line_chart(df.query('Avg_viewers'))
